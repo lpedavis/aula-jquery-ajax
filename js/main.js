b8e4e28eeaa7f4c59c863fde97681d4ce5df8e39
lpedavis <luizpaulodavis@hotmail.com>
2021-02-16 17:36:14 -0300

    adicionando projeto

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function consultar(){
+	let cep = document.querySelector("#cep").value
+	let url = "https://viacep.com.br/ws/"+ cep +"/json/";
+	console.log(url)
+	$.ajax({
+		url: url,
+		type: "GET", 
+		success: function (response){
+		$("#logradouro").html(response.logradouro);
+		$("#bairro").html(response.bairro);
+		$("#localidade").html(response.localidade);	
+		$("#uf").html(response.uf);
+		$("#ddd").html(response.ddd)
+		//document.querySelector("#logradouro").innerHTML= response.logradouro;
+		//document.querySelector("#bairro").innerHTML= response.bairro;
+		//document.querySelector("#localidade").innerHTML= response.localidade;
+		//document.querySelector("#uf").innerHTML= response.uf;
+		//document.querySelector("#ddd").innerHTML= response.ddd;
+
+
+			console.log(response)
+			//alert(response.logradouro)
+		}
+	})
+}
